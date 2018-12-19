c0a6bb2b826a4111bc0f86ce7b5d10dbce68f903
Elias Kotlyar <elias.kotlyar@mediawave.de>
2018-12-19 19:07:14 +0100

    BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,9 +69,10 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # TWRP stuff
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 DEVICE_RESOLUTION := 1024x600
-TARGET_SCREEN_HEIGHT := 600
 TARGET_SCREEN_WIDTH := 1024
-TW_THEME := landscape_mdpi
+TARGET_SCREEN_HEIGHT := 600
+
+# TW_THEME := landscape_mdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
@@ -99,3 +100,4 @@ TW_INCLUDE_CRYPTO := true
 
 
 RECOVERY_TOUCHSCREEN_SWAP_XY := true
+RECOVERY_TOUCHSCREEN_FLIP_Y := true
